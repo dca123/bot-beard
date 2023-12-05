c961427a05362b096f60891d8221cf7a9b6574e8
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2023-12-06 05:59:10 +0800

    added effect

--- generateDisplayName.ts
+++ generateDisplayName.ts
@@ -1,25 +1,44 @@
+import { Effect } from 'effect';
 import OpenAI from 'openai';
 
 const openai = new OpenAI({});
 
-export async function generateDisplayName(username: string) {
-  const chatCompletion = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    messages: [
-      {
-        role: 'system',
-        content:
-          'You are an assistant to create display names for a discord server. In this discord server, all the members have their names end with Beard. For example, DevBear, HenryBeard and AdamBeard. Your role is to create a display name given the user name of a person that joins the server',
-      },
-      {
-        role: 'user',
-        content: `Create a display name for ${username}`,
-      },
-    ],
+const chatCompletion = (username: string) =>
+  Effect.tryPromise(() =>
+    openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'system',
+          content:
+            'You are an assistant to create display names for a discord server. In this discord server, all the members have their names end with Beard. For example, DevBeard, HenryBeard and AdamBeard. Your role is to create a display name given the user name of a person that joins the server',
+        },
+        {
+          role: 'user',
+          content: `Create a display name for ${username}`,
+        },
+      ],
+    }),
+  );
+
+export function generateDisplayName(username: string) {
+  return Effect.gen(function* (_) {
+    const suggestions = yield* _(chatCompletion(username));
+    const displayName = suggestions.choices?.[0]?.message.content;
+    if (displayName === undefined) {
+      return yield* _(
+        Effect.fail(
+          `Display name is undefined - choices: ${suggestions.choices}`,
+        ),
+      );
+    }
+    if (displayName === null) {
+      return yield* _(
+        Effect.fail(
+          `Display name is null - message: ${suggestions.choices[0]}`,
+        ),
+      );
+    }
+    return displayName;
   });
-  const displayName = chatCompletion.choices[0].message.content;
-  if (displayName === null) {
-    throw new Error('Unable to generate display name');
-  }
-  return displayName;
 }
--- index.ts
+++ index.ts
@@ -1,23 +1,37 @@
 import { generateDisplayName } from './generateDisplayName';
-import { Client, Events, GatewayIntentBits } from 'discord.js';
+import { Client, Events, GatewayIntentBits, GuildMember } from 'discord.js';
+import { pipe, Effect } from 'effect';
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
 });
-async function main() {
-  // const displayName = await generateDisplayName('Johnny');
-  // console.log(displayName);
-  client.login(process.env.DISCORD_TOKEN);
-}
+
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
 
 client.on(Events.GuildMemberAdd, async (member) => {
-  console.log(member.displayName);
-  const suggestedName = await generateDisplayName(member.displayName);
-  member.setNickname(suggestedName);
-  console.log(suggestedName);
+  const result = await Effect.runPromiseExit(program(member));
+  if (result._tag === 'Failure') {
+    console.error(result);
+  }
 });
-client.on(Events.GuildMemberRemove, (member) => {
-  console.log(member);
-});
+
+const program = (member: GuildMember) =>
+  pipe(
+    generateDisplayName(member.displayName),
+    Effect.tap((displayName) =>
+      console.log(
+        `${new Date().toISOString()}: Changing user with displayName ${
+          member.displayName
+        } to ${displayName}`,
+      ),
+    ),
+    Effect.map((displayName) => member.setNickname(displayName)),
+  );
+
+function main() {
+  client.login(process.env.DISCORD_TOKEN);
+}
 
 main();
